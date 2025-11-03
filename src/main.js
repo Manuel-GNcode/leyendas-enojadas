7e60c9f5be579aedc71dc7829c93f44c45033e9a
GNcode <gngalarga027@gmail.com>
2025-11-03 15:00:23 -0500

    Refactor navigation and add responsive menu functionality with GSAP animations

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 const $btnRules = document.getElementById('btn-rules');
+const $btnMenu = document.getElementById('btn-menu');
+const $slideMenu = document.getElementById('slide-menu');
+const $navA =document.querySelectorAll('.nav-a');
 
 $btnRules.addEventListener('click', ()=>{
   const enlace = document.createElement("a");
@@ -6,3 +9,22 @@ $btnRules.addEventListener('click', ()=>{
   enlace.download = "instrucciones_leyendas_enojadas.pdf";
   enlace.click();
 })
+
+let menuHide = true;
+const tweenMenu = gsap.to($slideMenu, {
+  xPercent: -100,
+  duration: 0.4,
+  paused: true,
+})
+
+$btnMenu.addEventListener('click', ()=>{
+  if (menuHide) tweenMenu.restart();
+  else tweenMenu.reverse();
+  menuHide = !menuHide;
+})
+$navA.forEach(element => {
+  element.addEventListener('click', ()=>{
+    tweenMenu.reverse();
+    menuHide = !menuHide;
+  })
+});
