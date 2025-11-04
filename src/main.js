79469f32f4a19e6a4fe6a261b2b10abd98800245
GNcode <gngalarga027@gmail.com>
2025-11-04 13:50:41 -0500

    Add new logo image in WEBP format to the public directory

--- src/main.js
+++ src/main.js
@@ -1,30 +1,32 @@
-const $btnRules = document.getElementById('btn-rules');
-const $btnMenu = document.getElementById('btn-menu');
-const $slideMenu = document.getElementById('slide-menu');
-const $navA =document.querySelectorAll('.nav-a');
+document.addEventListener('DOMContentLoaded', () => {
+  const $btnRules = document.getElementById('btn-rules');
+  const $btnMenu = document.getElementById('btn-menu');
+  const $slideMenu = document.getElementById('slide-menu');
+  const $navA = document.querySelectorAll('.nav-a');
 
-$btnRules.addEventListener('click', ()=>{
-  const enlace = document.createElement("a");
-  enlace.href = "/docs/instrucciones_leyendas_enojadas.pdf";
-  enlace.download = "instrucciones_leyendas_enojadas.pdf";
-  enlace.click();
-})
+  $btnRules.addEventListener('click', () => {
+    const enlace = document.createElement("a");
+    enlace.href = "/docs/instrucciones_leyendas_enojadas.pdf";
+    enlace.download = "instrucciones_leyendas_enojadas.pdf";
+    enlace.click();
+  })
 
-let menuHide = true;
-const tweenMenu = gsap.to($slideMenu, {
-  xPercent: -100,
-  duration: 0.4,
-  paused: true,
-})
+  let menuHide = true;
+  const tweenMenu = gsap.to($slideMenu, {
+    xPercent: -100,
+    duration: 0.4,
+    paused: true,
+  })
 
-$btnMenu.addEventListener('click', ()=>{
-  if (menuHide) tweenMenu.restart();
-  else tweenMenu.reverse();
-  menuHide = !menuHide;
-})
-$navA.forEach(element => {
-  element.addEventListener('click', ()=>{
-    tweenMenu.reverse();
+  $btnMenu.addEventListener('click', () => {
+    if (menuHide) tweenMenu.restart();
+    else tweenMenu.reverse();
     menuHide = !menuHide;
   })
-});
+  $navA.forEach(element => {
+    element.addEventListener('click', () => {
+      tweenMenu.reverse();
+      menuHide = !menuHide;
+    })
+  });
+})
